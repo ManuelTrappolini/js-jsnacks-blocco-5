e646e97336e0d43cce0ed1c847f7d35095528447
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-16 14:03:15 +0200

    add fourth snack

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorName = posts.map((post) => post.author)
+console.log(authorName);
+
