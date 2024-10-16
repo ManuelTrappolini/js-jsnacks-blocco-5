7bfdb6fd4b047d5c04821b8294ec21813683139d
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-16 14:32:27 +0200

    add exercise bonus five

--- Bonus/ex-js-array-methods-main/snack-5/script.js
+++ Bonus/ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,14 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+let newArr = []
+
+for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if(num % 2 === 0){
+        newArr.push(num)
+    }
+    
+}
+console.log(newArr);
