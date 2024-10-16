c3cb6ed9c76f36d865aee8af8159d75355dcabbe
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-16 14:38:56 +0200

    add snack bonus seven

--- Bonus/ex-js-array-methods-main/snack-7/script.js
+++ Bonus/ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,12 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+for (let i = 0; i < students.length; i++) {
+  const student = students[i];
+  if(student.id > 1 && student.id < 3){
+  console.log(student);
+    
+    
+  }
+}
