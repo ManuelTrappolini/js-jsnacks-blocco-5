0d16592517ce970e2c88878a021cbb7798ad47d0
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-16 14:30:31 +0200

    add exercise bonus four

--- Bonus/ex-js-array-methods-main/snack-4/script.js
+++ Bonus/ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,12 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+let newArr = []
+
+for (let i = 0; i < posts.length; i++) {
+  const post = posts[i];
+  newArr.push(post.author)
+  
+}
+console.log(newArr);
