a3c14914b3500a52bf646179d0ca26a1368ca1bb
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-16 14:36:08 +0200

    add exercise bonus six

--- Bonus/ex-js-array-methods-main/snack-6/script.js
+++ Bonus/ex-js-array-methods-main/snack-6/script.js
@@ -12,3 +12,18 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+let zucchineLunghe = []
+let zucchineCorte = []
+
+for(let i = 0; i < zucchine.length; i++){
+  const zucchina = zucchine[i]
+  if(zucchina.length >= 15){
+    zucchineLunghe.push(zucchina)
+  }
+  else{
+    zucchineCorte.push(zucchina)
+  }
+}
+console.log(zucchineLunghe);
+console.log(zucchineCorte);
